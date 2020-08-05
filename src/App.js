2bc580a493fed9d6c33612bec92378811da78f25
Asadullah1996 <Asadullah1996@gmail.com>
2020-08-06 01:23:04 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        By Asadullah
+        <h1>By Asadullah</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
